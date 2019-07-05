f660a5c2cc2e8deb419439562435e2bc3d1844c9
Team3-Derivco <52511097+Team3-Derivco@users.noreply.github.com>
2019-07-05 11:37:42 +0200

    Team 3 quiz

--- summary.js
+++ summary.js
@@ -1,18 +1,17 @@
 var congratsHeader = document.querySelector('#congratsHeader');
 var score = document.querySelector('#score');
 
-if(!localStorage.getItem('user')){
+if (!localStorage.getItem('user')) {
     location.href = "index.html";
-}else{
-    congratsHeader.textContent = `Congulations ${localStorage.getItem('user')}`; 
-    score.innerHTML = `Your Score is ${localStorage.getItem('score')}`;
+} else {
+    congratsHeader.textContent = `Congulations ${localStorage.getItem('user')}`;
+    score.innerHTML = `Your Score is ${parseInt(localStorage.getItem('score'))}`;
 }
 
-function leaderboard(){ 
+function leaderboard() {
     location.href = "leaderboard.html";
 }
 
-function playAgain(){
+function playAgain() {
     location.href = "login.html";
 }
-
